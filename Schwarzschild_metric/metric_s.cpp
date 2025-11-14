19165c297d3a94b8d63ca973e344ce531ba60cd4
Fiaalho <jonny.victor23@gmail.com>
2025-11-14 19:40:32 -0300

    Modifiquei pouca coisa no codigo

--- Schwarzschild_metric/metric_s.cpp
+++ Schwarzschild_metric/metric_s.cpp
@@ -1,19 +1,18 @@
 #include<iostream>
 #include<cmath>
-#include<vector>
 #include<fstream>
 
 using namespace std;
 
 class SchwarszschildMetric {
   private:
-    double M; //massa do buraco negro
-    double rs; //raio de Schwarszschild
+    double M; //mass of the black hole
+    double rs; //Schwarzschild radius
 
   public:
-    SchwarszschildMetric(double mass) : M(mass) , rs(2 * mass) {}//construtor
+    SchwarszschildMetric(double mass) : M(mass) , rs(2 * mass) {}//constructor
     
-  //métodos da classe
+  //class methods
   
   double gtt(double r) {
       return -(1.0 - rs / r);
@@ -31,37 +30,35 @@ class SchwarszschildMetric {
       return r * r * sin(theta) * sin(theta);
     }
 
-  //gerar os dados da métrica em um arquivo
+  //generate metric data in a file
   void generateMetricData(const string& filename){
     ofstream file(filename);
 
-    file << "# r g_tt g_rr g_theta_theta g_phi_phi\n";//escreve no arquivo o cabeçalho
-
+    file << "# r g_tt g_rr g_theta_theta g_phi_phi\n";//write header to file
     for(double r = rs + 0.1; r <= rs + 10.0; r += 0.1){
-      double g_tt = gtt(r);//variavel g_tt recebe o valor retornado pela função gtt
-      double g_rr = grr(r);//variavel g_rr recebe o valor retornado pela função grr
-      double g_theta_theta = gtheta_theta(r);//variavel g_theta_theta recebe o valor retornado pela função gtheta_theta
-      double g_phi_phi = gphi_phi(r, M_PI / 4); //M_PI é o valor de pi incluso em cmath
+      double g_tt = gtt(r);//variable g_tt receives the value returned by the function gtt
+      double g_rr = grr(r);//variable g_rr receives the value returned by the function grr
+      double g_theta_theta = gtheta_theta(r);//variable g_theta_theta receives the value returned by the function gtheta_theta
+      double g_phi_phi = gphi_phi(r, M_PI / 4); //M_PI is the value of pi included in cmath
 
-      file << r << " " << g_tt << " " << g_rr << " " << g_theta_theta << " " << g_phi_phi << "\n";//escreve os valores para cada cooordenada da métrica no arquivo
+      file << r << " " << g_tt << " " << g_rr << " " << g_theta_theta << " " << g_phi_phi << "\n";//write the values for each metric coordinate to the file
     }
 
-    file.close();//fecha o arquivo
+    file.close();//close the file
   }   
 };
 
 int main() {
 
-  SchwarszschildMetric metrica(1);//cria um objeto da classe SchwarszschildMetric com o nome metrica passando a massa fornecida
-  metrica.generateMetricData("schwarszschild_metric.dat");//gera os dados da métrica pelo método generateMetricData
+  SchwarszschildMetric metric(1);//create an object of the class SchwarszschildMetric named metric passing the provided mass
+  metric.generateMetricData("schwarszschild_metric.dat");//generate metric data using the generateMetricData method
 
-  cout << "Dados da métrica gerados em schwarszschild_metric.dat" << endl;
+  cout << "Metric data generated in schwarszschild_metric.dat" << endl;
 
+  SchwarszschildMetric metric2(2);//object metric2 to access the SchwarzschildMetric class for M=2
+  metric2.generateMetricData("schwarszschild_metric2.dat");
 
-  SchwarszschildMetric metrica2(2);//objeto metrica2 para acessar a classe SchwarzschildMetric para M=2
-  metrica2.generateMetricData("schwarszschild_metric2.dat");
-
-  cout << "\nDados da métrica gerados em schwarszschild_metric2.dat" << endl;
+  cout << "\nMetric data generated in schwarszschild_metric2.dat" << endl;
 
   return 0;
 }
