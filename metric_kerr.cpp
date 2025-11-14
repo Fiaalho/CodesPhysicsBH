453443b8e470f7657ae5a2653b2060355203d0ea
Fiaalho <jonny.victor23@gmail.com>
2025-11-14 19:13:20 -0300

    Codigos em C++ adicionados

---
+++ metric_kerr.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<cmath>
+#include<fstream>
+
+using namespace std;
+
+class KerrMetric {
+  private:
+    double M;
+    double a;
+    double r;//raio do horizonte
+    
+  public:
+    KerrMetric(double mass, double spin): M(mass), a(spin), r(mass + sqrt(mass*mass - spin*spin)) {}//construtor
+
+
+    //valor de sigma
+    double sigma(double r, double theta) {
+        return r*r + a*a * cos(theta)*cos(theta);
+    }
+
+    double delta(double r){
+        return (r*r) - 2*M*r + a*a;
+    }
+
+    double g_tt(double r, double theta) {
+        double sig = sigma(r, theta);
+        return -(1 - (2*M*r)/sig);
+    }
+
+    double g_tphi(double r, double theta) {
+        double sig = sigma(r, theta);
+        return -(4*M*r*a*sin(theta)*sin(theta))/sig;
+    }
+
+    double g_rr(double r, double theta) {
+        double sig = sigma(r, theta);
+        double del = delta(r);
+        return sig/del;
+    }
+
+    double g_thetatheta(double r, double theta) {
+        double sig = sigma(r, theta);
+        return sig;
+    }
+
+    double g_phiphi(double r, double theta) {
+        double sig = sigma(r, theta);
+        double del = delta(r);
+        return (sin(theta)*sin(theta)) * (r*r + a*a)*(r*r + a*a)-a*a*del*sin(theta)*sin(theta)/sig;
+    }
+
+    //gerar arquivo com os dados das componentes da métrica
+    void generateMetricData(const string& filename){
+      ofstream file(filename);
+
+      file << "r gtt gtphi grr gthetatheta gphiphi\n";
+
+      double theta = M_PI/6;
+
+      for(double rn=r+0.01; rn<=r+10.0; rn+=0.1){
+        double gtt = g_tt(rn, theta);
+        double gtphi = g_tphi(rn, theta);
+        double grr = g_rr(rn, theta);
+        double gthetatheta = g_thetatheta(rn, theta);
+        double gphiphi = g_phiphi(rn, theta);
+
+        file << rn << " " << gtt << " " << gtphi << " " << grr << " " << gthetatheta << " " << gphiphi << "\n";
+      }
+      file.close();
+    }
+};
+
+int main(){
+  KerrMetric k(1.0, 0.6);
+  k.generateMetricData("kerr_metric.dat");
+
+  KerrMetric k2(2.0, 1.0);
+  k2.generateMetricData("kerr_metric_2.dat");
+}
